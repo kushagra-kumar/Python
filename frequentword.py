3d0cd9448339bc4ee3b31aea93d10a4d754f87fc
kushagra-kumar <kushagrakumar89@gmail.com>
2020-02-23 15:09:39 +0100

    Base code for counting top 10 most frequent words in a text file.

---
+++ frequentword.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import argparse
+
+# To run the program -> python frequentword.py --filepath <file_path>
+# e.g. python frequentword.py --filepath "C:\Users\lenovo\Desktop\Python\code"
+
+
+parser = argparse.ArgumentParser(description="Parse the input file path")
+parser.add_argument('--filepath',type=str,required=True,help="Input file")
+args = parser.parse_args()
+file_path = args.filepath
+#print("file_path")
+
+FH = open("story","r")
+text = FH.read()
+words = text.split()
+#print(text.split())
+
+# Key = word , value = word count
+wordsdict = {}
+
+# store the word and it's count in the dictionary.
+for ele in words:
+    wordsdict[ele] = words.count(ele)
+
+# used code snippet from Day 5 Tutorial.
+def get_key(key):
+    return wordsdict[key]
+
+i = 1
+for word in sorted(wordsdict,key=get_key,reverse=True):
+    print(word,wordsdict[word])
+    if i == 10:
+        break
+    else:
+        i += 1
+
+FH.close()
